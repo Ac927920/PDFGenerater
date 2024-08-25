bd86bd1e27ce6964b0b2666e6f0bb428d7d918d8
Abhishek Chandra <ac927920@gmail.com>
2024-08-26 02:56:18 +0530

    Manual Commit

---
+++ app/src/main/java/com/abhishek/generatepdfbylayout/Product.kt
@@ -0,0 +1,12 @@
+package com.abhishek.generatepdfbylayout
+
+data class Product(
+    val serialNumber: Int,
+    val description: String,
+    val quantity: Int,
+    val rate: Int,
+    val amount: Int
+)
+
+// Add Repo without Git Add <?> Commands
+
